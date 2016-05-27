f019ed50c6956edf72ee42c970ef80250ab8a49a
Mick Hakobyan <mhakobyan@netflix.com>
2016-05-27 08:46:34 -0700

    It's now HTML with tags and basic styles.

--- chnobean.jsonviewer.js
+++ chnobean.jsonviewer.js
@@ -36,64 +36,70 @@
     // guard against undefined being re-defined
     var undefined;
 
-    var indent;
-
     // main public method
     function createRootElement(json) {
-        indent = '';
-        var content = createElement(null, json);
-        return createHTMLElement('pre', null, content);
+        var content = create(null, json, '');
+        return createHTMLElement('pre', content);
     }
 
     // forward to the correct create function
-    function createElement(key, o) {
+    function create(key, o, comma) {
         var type = typeofEx(o);
-        return createElementByType[type](key, o);
+        return createByType[type](key, o, comma);
     }
 
     // each type has it's own creator
-    var createElementByType = {
-        'string': function(key, o) {
-            return createKeyElement(key) + '"' + o + '"';
+    var createByType = {
+        
+        'string': function(key, o, comma) {
+            return [createKeyElement(key), span('"' + o + '"', 'string'), comma];
         },
 
-        'number': function(key, o) {
-            return createKeyElement(key) + o;
+        'number': function(key, o, comma) {
+            return [createKeyElement(key), span(o, 'number'), comma];
         },
 
-        'null': function(key) {
-            return createKeyElement(key) + 'null';
+        'null': function(key, o, comma) {
+            return [createKeyElement(key), 'null', comma];
         },
 
-        'undefined': function(key, o) {
-            return createKeyElement(key) + 'undefined';
+        'undefined': function(key, o, comma) {
+            return [createKeyElement(key), 'undefined', comma];
         },
 
-        'array': function(key, o) {
-            indent += '  ';
-            var inner = o.map(function(ao){ 
-                return createElement(null, ao);
-            }).join(',\n');
-            indent = indent.substring(2);
-            return createKeyElement(key, '[\n') + inner + '\n' + indent + ']';
+        'array': function(key, o, comma) {
+            var length = o.length;
+            var index = 0;
+            var r = createKeyElement(key, '[')
+                .concat(o.map(function(ao){ 
+                    var last = (++index) == length;
+                    return div(create(null, ao, last ? '' : ','));
+                }))
+                .concat(']' + comma);
+            return r;
         },
 
-        'object': function(key, o) {
-            indent += '  ';
-            var inner = Object.keys(o).map(function(k){ 
-                return createElement(k, o[k]); 
-            }).join(',\n');
-            indent = indent.substring(2);
-            return createKeyElement(key, '{\n') + inner + '\n' + indent + '}';
+        'object': function(key, o, comma) {
+            var keys = Object.keys(o);
+            var length = keys.length;
+            var index = 0;
+            var r = createKeyElement(key, '{')
+                .concat(keys.map(function(k){ 
+                    var last = (++index) == length;
+                    return div(create(k, o[k], last ? '' : ',')); 
+                }))
+                .concat('}' + comma);
+            return r;
         }
 
     };
 
     // creates the "key" part of sub-elements
     function createKeyElement(key, openBracket) {
-        return indent + 
-            (key ? ('"' + key + '": ') : '' ) +
-            (openBracket ? openBracket : '');
+        return [
+            key ? span('"' + key + '": ', 'key') : null,
+            openBracket ? span(openBracket) : null
+        ];
     }
 
     // expanded version of typeof, which also supports null and an array
@@ -107,11 +113,33 @@
         }
     }
 
-    function createHTMLElement(tag, className, content) {
+    function span(content, className) {
+        return createHTMLElement('span', content, className);
+    }
+
+    function div(content, className) {
+        return createHTMLElement('div', content, className);
+    }
+
+    function createHTMLElement(tag, content, className) {
         var el = document.createElement(tag);
-        if (className) el.className = className;
-        if (content) el.textContent = content;
+        if (className) { 
+            el.className = className;
+        }
+        appendContent(el, content);
         return el;
+    }
+
+    function appendContent(el, content) {
+        if (content) {
+            if (content.nodeType > 0) {
+                el.appendChild(content)
+            } else if (Array.isArray(content)) {
+                content.forEach(function(c){appendContent(el, c)});
+            } else {
+                el.appendChild(document.createTextNode(content));
+            }
+        }
     }
 
     // the public interface
